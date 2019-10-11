bd09d2912b1e4abccdd54d725d37ae21d2cff866
forrestlin <forrestlin@lexinfintech.com>
2019-10-11 20:07:58 +0800

    接雨水

---
+++ src/main/java/org/forrestlin/leetcode/L42TrappingRainWater.java
@@ -0,0 +1,11 @@
+package org.forrestlin.leetcode;
+
+/**
+ * @program: leetcode
+ * @description: 接雨水
+ * @author: forrestlin
+ * @create: 2019-10-10 10:47
+ */
+public class L42TrappingRainWater {
+
+}
