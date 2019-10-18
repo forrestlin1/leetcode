3e27b099912b183485e5f8a922d7fd2a1fb7a44a
qmaker <qmaker2163.com>
2019-10-18 22:47:14 +0800

    leetcode

--- src/main/java/org/forrestlin/leetcode/L75SortColors.java
+++ src/main/java/org/forrestlin/leetcode/L75SortColors.java
@@ -1,6 +1,7 @@
 package org.forrestlin.leetcode;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @program: leetcode
@@ -12,11 +13,20 @@ public class L75SortColors {
 
     public static void main(String[] args) {
         L75SortColors test = new L75SortColors();
-        int[] a = new int[]{1,2,0};
+        int[] a = new int[]{1, 2, 0};
         test.sortColors(a);
-        System.out.println(Arrays.asList(a));
-    }
 
+        for (int i : a) {
+
+            System.out.println(i);
+        }
+
+    }
+/**
+ * 我们用三个指针（left, right 和cur）来分别追踪0的最右边界，2的最左边界和当前考虑的元素,向右遍历树组，遇到0则和left替换
+ * 遇到2则和right替换。
+ *
+ * */
     public void sortColors(int[] nums) {
         if (null == nums || nums.length <= 1) {
             return;
@@ -26,16 +36,19 @@ public class L75SortColors {
         while (cur <= right) {
 
             if (nums[cur] == 0) {
-                nums[cur++] = nums[left];
+                nums[cur] = nums[left];
                 nums[left] = 0;
                 left++;
-            }
-            if (nums[cur] == 2) {
+                //注意   这里只有等于0时候需要cur++因为左边是指针遍历过了，没问题的数组，而右边替换后，可能把一个0替换回来
+                //这个时候就需要继续替换了
+                cur++;
+            } else if (nums[cur] == 2) {
                 nums[cur] = nums[right];
                 nums[right] = 2;
                 right--;
-            } else {
+            } else if (nums[cur] == 1) {
                 cur++;
+
             }
 
         }
