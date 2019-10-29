1fb008d6c92dbbf62ef8e1858d0a5a61c2ad12bd
forrestlin <forrestlin@lexinfintech.com>
2019-10-29 19:40:16 +0800

    leetcode

---
+++ src/main/java/org/forrestlin/leetcode/L200NumberOfIslands.java
@@ -0,0 +1,15 @@
+package org.forrestlin.leetcode;
+
+/**
+ * @program: leetcode
+ * @description: 岛屿数量
+ * @author: forrestlin
+ * @create: 2019-10-29 15:56
+ */
+public class L200NumberOfIslands {
+
+    public int numIslands(char[][] grid) {
+
+        return 0;
+    }
+}
