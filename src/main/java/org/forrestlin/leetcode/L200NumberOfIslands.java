a91db3d3366c7dd407e8502492105e18a6279e54
forrestlin1 <qmaker2163.com>
2019-10-30 22:05:02 +0800

    leetcode

--- src/main/java/org/forrestlin/leetcode/L200NumberOfIslands.java
+++ src/main/java/org/forrestlin/leetcode/L200NumberOfIslands.java
@@ -1,5 +1,7 @@
 package org.forrestlin.leetcode;
 
+import java.util.LinkedList;
+
 /**
  * @program: leetcode
  * @description: 岛屿数量
@@ -8,8 +10,101 @@ package org.forrestlin.leetcode;
  */
 public class L200NumberOfIslands {
 
+    /*
+     * dfs,深度优先搜索，遍历的时候把grid赋值为0
+     * */
     public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int islands = 0;
+        for (int r = 0; r <= grid.length; r++) {
+            for (int c = 0; c <= grid[0].length; c++) {
+                if (grid[r][c] == '1') {
+                    islands++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
 
         return 0;
     }
+
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+
+    private int rows;
+    private int cols;
+
+    /*
+     * 广度优先bfs,需要一个队列存储待遍历的坐标
+     * */
+    public int numIslands2(char[][] grid) {
+        //           x-1,y
+        //  x,y-1    x,y      x,y+1
+        //           x+1,y
+        int[][] directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+        rows = grid.length;
+        if (rows == 0) {
+            return 0;
+        }
+        cols = grid[0].length;
+        boolean[][] marked = new boolean[rows][cols];
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                // 如果是岛屿中的一个点，并且没有被访问过
+                // 从坐标为 (i,j) 的点开始进行广度优先遍历
+                if (!marked[i][j] && grid[i][j] == '1') {
+                    count++;
+                    LinkedList<Integer> queue = new LinkedList<>();
+                    // 小技巧：把坐标转换为一个数字
+                    // 否则，得用一个数组存，在 Python 中，可以使用 tuple 存
+                    queue.addLast(i * cols + j);
+                    // 注意：这里要标记上已经访问过
+                    marked[i][j] = true;
+                    while (!queue.isEmpty()) {
+                        int cur = queue.removeFirst();
+                        int curX = cur / cols;
+                        int curY = cur % cols;
+                        // 得到 4 个方向的坐标，TODO 获取四个方向的方法值得借鉴
+                        for (int k = 0; k < 4; k++) {
+                            int newX = curX + directions[k][0];
+                            int newY = curY + directions[k][1];
+                            // 如果不越界、没有被访问过、并且还要是陆地，我就继续放入队列，放入队列的同时，要记得标记已经访问过
+                            if (inArea(newX, newY) && grid[newX][newY] == '1' && !marked[newX][newY]) {
+                                queue.addLast(newX * cols + newY);
+                                // 【特别注意】在放入队列以后，要马上标记成已经访问过，语义也是十分清楚的：反正只要进入了队列，你迟早都会遍历到它
+                                // 而不是在出队列的时候再标记
+                                // 【特别注意】如果是出队列的时候再标记，会造成很多重复的结点进入队列，造成重复的操作，这句话如果你没有写对地方，代码会严重超时的
+                                marked[newX][newY] = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        return count;
+    }
+
+    private boolean inArea(int x, int y) {
+        // 等于号这些细节不要忘了
+        return x >= 0 && x < rows && y >= 0 && y < cols;
+    }
+
+
 }
