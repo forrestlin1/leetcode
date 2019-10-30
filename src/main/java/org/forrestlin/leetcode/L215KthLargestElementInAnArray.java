698d3a719ce069ffea47b32a5e0081f857579f80
forrestlin1 <qmaker2163.com>
2019-10-30 22:20:56 +0800

    leetcode

---
+++ src/main/java/org/forrestlin/leetcode/L215KthLargestElementInAnArray.java
@@ -0,0 +1,30 @@
+package org.forrestlin.leetcode;
+
+import java.util.PriorityQueue;
+
+/**
+ * 数组中最K大的元素
+ */
+public class L215KthLargestElementInAnArray {
+
+    public static void main(String[] args) {
+        L215KthLargestElementInAnArray test = new L215KthLargestElementInAnArray();
+        System.out.print(test.findKthLargest(new int[]{1, 4, 5, 2, 3}, 3));
+    }
+
+
+    /**
+     * 使用优先队列
+     */
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int num : nums) {
+            q.add(num);
+            if (q.size() > k) {
+                q.poll();
+            }
+        }
+        return q.peek();
+
+    }
+}
